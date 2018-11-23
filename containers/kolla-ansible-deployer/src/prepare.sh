a05ce5325e11e0212d7a6450d6a612a0afee287d
Andrey Pavlov <andrey.mp@gmail.com>
2018-11-23 17:45:56 +0300

    DNM: test zuul jobs
    Change-Id: Iede328d8593df7356c0eb29466ff3f9778588ddd
    Partial-Bug: #1803119
    Depends-On: Ib5a4367fdeef19c8d1bf5801b676caff95383bd2

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -3,5 +3,6 @@
 yum install -y git
 
 cd /root
+ls -lR
 [ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer -b master
 [ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible -b master
