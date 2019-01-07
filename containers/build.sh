964bc0bf94d268d481f2d019d23d15af6d6b99b3
Andrey Pavlov <andrey.mp@gmail.com>
2019-01-07 16:38:57 +0300

    remove installing epel package from all containers
    introduce deployers-base and inherit all deployers from it.
    install python-pip in style as it present in mainline build
    
    Change-Id: Ia61953ed81fb07bb7d7c619cceafe240b019968e
    closes-jira-bug: JCB-218785

--- containers/build.sh
+++ containers/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds containers. Takes CONTRAIL_REGISTRY, CONTRAIL_DEPLOYERS_TAG, CONTRAIL_CONTAINER_TAG, LINUX_DISTR, LINUX_DISTR_VER from environment.
+# Builds containers. Takes CONTRAIL_REGISTRY, CONTRAIL_DEPLOYERS_TAG, LINUX_DISTR, LINUX_DISTR_VER from environment.
 # Parameters:
 # path: relative path (from this directory) to module(s) for selective build. Example: ./build.sh kolla-ansible-deployer
 #   if it's omitted then script will build all
@@ -50,14 +50,18 @@ function process_container() {
     # and then change FROM-s that uses ARG-s
     sed -i \
       -e "s|^FROM \${DEPLOYERS_BASE_CONTAINER}|FROM ${DEPLOYERS_BASE_CONTAINER}|" \
+      -e "s|^FROM \${CONTRAIL_REGISTRY}/\([^:]*\):\${CONTRAIL_DEPLOYERS_TAG}|FROM ${CONTRAIL_REGISTRY}/\1:${tag}|" \
       -e "s|^FROM \$LINUX_DISTR:\$LINUX_DISTR_VER|FROM $LINUX_DISTR:$LINUX_DISTR_VER|" \
       ${docker_file}.nofromargs
     docker_file="${docker_file}.nofromargs"
   fi
   build_arg_opts+=" --build-arg CONTRAIL_REGISTRY=${CONTRAIL_REGISTRY}"
+  build_arg_opts+=" --build-arg CONTRAIL_DEPLOYERS_TAG=${tag}"
   build_arg_opts+=" --build-arg DEPLOYERS_BASE_CONTAINER=${DEPLOYERS_BASE_CONTAINER}"
   build_arg_opts+=" --build-arg LINUX_DISTR_VER=${LINUX_DISTR_VER}"
   build_arg_opts+=" --build-arg LINUX_DISTR=${LINUX_DISTR}"
+  [ -n "$PYTHON_PIP_RPM" ] && build_arg_opts+=" --build-arg PYTHON_PIP_RPM=$PYTHON_PIP_RPM"
+  [ -n "$PYTHON_PIP_VENV" ] && build_arg_opts+=" --build-arg PYTHON_PIP_VENV=$PYTHON_PIP_VENV"
   build_arg_opts+=" --build-arg CONTAINER_NAME=${container_name}"
 
   local logfile='build-'$container_name'.log'
@@ -82,7 +86,7 @@ function process_dir() {
     return
   fi
   for d in $(ls -d $dir/*/ 2>/dev/null); do
-    if [[ $d != "./" && $d == */general-base* ]]; then
+    if [[ $d != "./" && $d == */deployers-base* ]]; then
       process_dir $d
     fi
   done
