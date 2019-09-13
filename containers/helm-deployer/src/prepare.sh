b6a06acde28c5cf7792790a6640e269571ae0e45
Andrey Pavlov <andrey.mp@gmail.com>
2019-09-13 04:12:40 -0700

    dnm: test
    Change-Id: Idfd6a8d344798e0a776bc85d407655279ed90c10

--- containers/helm-deployer/src/prepare.sh
+++ containers/helm-deployer/src/prepare.sh
@@ -1,6 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cd /root
+ls -l
 [ -d contrail-helm-deployer ] || git clone https://github.com/Juniper/contrail-helm-deployer -b master
 [ -d openstack-helm ] || git clone https://github.com/Juniper/openstack-helm -b master
 [ -d openstack-helm-infra ] || git clone https://github.com/Juniper/openstack-helm-infra -b master
