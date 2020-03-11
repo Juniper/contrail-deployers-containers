fc7cc88bf59a54b050c5dfd23b7d142a2f76f81c
Andrey Pavlov <andrey-mp@yandex.ru>
2020-03-11 18:24:03 +0300

    remove hack for kolla-deployer container
    Change-Id: Id569584379f45d77ba85115393165dae190cb1de

--- containers/kolla-ansible-deployer/src/prepare.sh
+++ containers/kolla-ansible-deployer/src/prepare.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
 cd /root
+<<<<<<< HEAD   (fadffb pin packages for openshift-deployer)
+=======
+
+>>>>>>> CHANGE (a15ae1 remove hack for kolla-deployer container)
 [ -d contrail-ansible-deployer ] || git clone https://github.com/Juniper/contrail-ansible-deployer -b master
 [ -d contrail-kolla-ansible ] || git clone https://github.com/Juniper/contrail-kolla-ansible -b master
