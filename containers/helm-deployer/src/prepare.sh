e7b6ef446dc933ea31b2b2bfcfbfbb53b4f636d6
Andrey Pavlov <andrey-mp@yandex.ru>
2020-03-11 22:26:02 +0300

    [Migration/Juniper/contrail-helm-deployer] Change links from to tungstenfabric/tf-helm-deployer
    Automated change
    Depends-On: I70145534968cf395d3ca10266c69d22a32717b68
    
    Change-Id: Ic7f8309e1fe2ffedf9a14ed04ba41ed1acd37a08

--- containers/helm-deployer/src/prepare.sh
+++ containers/helm-deployer/src/prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 cd /root
-[ -d contrail-helm-deployer ] || git clone https://github.com/Juniper/contrail-helm-deployer -b master
+[ -d contrail-helm-deployer ] || git clone https://github.com/tungstenfabric/tf-helm-deployer -b master
 [ -d openstack-helm ] || git clone https://github.com/Juniper/openstack-helm -b master
 [ -d openstack-helm-infra ] || git clone https://github.com/Juniper/openstack-helm-infra -b master
